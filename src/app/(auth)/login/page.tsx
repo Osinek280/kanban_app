23a3077561473025c4bfb71ba65912f375d75568
Szymon <szymon.osinski.2009@gmail.com>
2024-01-24 16:22:37 +0100

    added login and register modal

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,34 @@
+import Link from "next/link";
 import HomeLayout from "@/app/homeLayout";
+import styles from "../loginRegister.module.css";
 
 const Login = () => {
-  return(
+  return (
     <HomeLayout>
-      Login
+      <div className={styles["login-box"]}>
+        <h2>Login</h2>
+        <form>
+          <div className={styles["user-box"]}>
+            <input type="text" name="username" required />
+            <label>Username</label>
+          </div>
+          <div className={styles["user-box"]}>
+            <input name="password" required />
+            <label>Password</label>
+            <span className={styles.deputy}></span>
+          </div>
+          <div className={styles["remember-me"]}>
+            <input type="checkbox" id="remember-me-check-box" name="rememberMe" />
+            <label htmlFor="remember-me-check-box">Remember me</label>
+          </div>
+          <div className={styles["toggle_label"]}>
+            <Link href="/register">Sign up</Link>
+          </div>
+          <button type="submit" className={styles["submit-btn"]}>
+            Submit
+          </button>
+        </form>
+      </div>
     </HomeLayout>
   );
 };
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,9 +1,48 @@
+import Link from "next/link";
 import HomeLayout from "@/app/homeLayout";
+import styles from "../loginRegister.module.css";
 
 const Register = () => {
-  return(
+  return (
     <HomeLayout>
-      Register
+      <div className={styles["register-box"]}>
+        <h2>Register</h2>
+        <form>
+          <div className={styles["user-box"]}>
+            <input
+              type="text"
+              name="username"
+              required
+            />
+            <label>Username</label>
+          </div>
+          <div className={styles["user-box"]}>
+            <input
+              type="email"
+              name="email"
+              required
+            />
+            <label>Email</label>
+          </div>
+          <div className={styles["user-box"]}>
+            <input
+              name="password"
+              required
+            />
+            <label>Password</label>
+            <span
+              className={styles.deputy}
+            >
+            </span>
+          </div>
+          <div className={styles["toggle_label"]}>
+            <Link href={'/login'}>Login</Link>
+          </div>
+          <button type="submit" className={styles["submit-btn"]}>
+            Submit
+          </button>
+        </form>
+      </div>
     </HomeLayout>
   );
 };
